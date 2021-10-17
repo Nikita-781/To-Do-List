2d00733f5c03ba268f1db4e5b44abd3d6a1c3fe6
Nikita-781 <85981347+Nikita-781@users.noreply.github.com>
2021-10-17 23:42:13 +0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+let notes = {
+    monday: ['note1', 'note1'],
+    tuesday: ['note2', 'note2', 'note2', 'note2', 'note2', 'note2', 'note2', 'note2'],
+    wednesday: ['note3', 'note3'],
+    thursday: ['note4', 'note4'],
+    friday: ['note5', 'note5'],
+    saturday: ['note6', 'note6'],
+    sunday: ['note7', 'note7']
+}
+
+const block_notes = document.querySelector('.n-notes');
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(el => {
+    el.addEventListener('click', function () {
+        this.classList.add('active');
+        let dataDay = [];
+        removeNotes();
+        for (const key in notes) {
+            if (this.getAttribute('data-value') == key) {
+                dataDay = notes[key];
+                if (dataDay.length > 7) {
+                    getScrollBlock(block_notes);
+                }
+                for (const val in dataDay) {
+                    let item = document.createElement('div');
+                    item.classList.add('item');
+                    item.innerHTML = dataDay[val];
+                    block_notes.append(item);
+                }
+            }
+        }
+    });
+});
+
+function removeNotes() {
+    block_notes.innerHTML = '';
+}
+
+function getScrollBlock(ele) {
+    ele.classList.toggle('scroll');
+}
+
